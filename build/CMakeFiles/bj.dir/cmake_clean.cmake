2425981880a56f31140cefd8331bbabf8af9cafa
XsaTi <latgopro@gmail.com>
2026-07-14 02:13:54 +0300

    Repo

---
+++ build/CMakeFiles/bj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bj.dir/link.d"
+  "CMakeFiles/bj.dir/src/main.cpp.o"
+  "CMakeFiles/bj.dir/src/main.cpp.o.d"
+  "bj"
+  "bj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Nice to see you in the game of 21!" "\n" "Here you can play against the computer and try to win!" "\n" "Good luck to you!";
+    return 0;
+}
